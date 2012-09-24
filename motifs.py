dfa75021e43569996def532676a5c250e004ada3
Ethan Sherbondy <sherbondye@gmail.com>
2012-09-23 22:36:29 -0500

    motif finding code implemented

--- motifs.py
+++ motifs.py
@@ -3,19 +3,21 @@
 
 import re
 from collections import Counter
+from operator import itemgetter
 
 def find_motifs():
   inter_data = ""
   motifs = Counter()
   conserved_motifs = Counter()
+  conservation_ratios = {}
 
   with open("genes/allinter") as f:
     inter_data = f.read()
 
   motifs.update([inter_data[i:(i+6)] for i in range(len(inter_data)-6)])
 
-  print "Total number of motifs: {0}".format(len(motifs))
-  print "Most frequent motifs:"
+  print "Total number of hexamers: {0}".format(len(motifs))
+  print "50 most frequent motifs:"
   print motifs.most_common(50)
     
   with open("genes/allintercons") as f:
@@ -25,13 +27,18 @@ def find_motifs():
       motif = inter_data[match.start(1):match.end(1)]
       conserved_motifs.update([motif])
 
-    print "Total number of hexamers: {0}".format(sum(conserved_motifs.values()))
-    print "Most frequent conserved motifs:"
-    return conserved_motifs.most_common(50)
+    for motif in conserved_motifs:
+      conservation_ratios[motif] = float(conserved_motifs[motif]) / motifs[motif]
+
+    print "Total number of conserved hexamers: {0}".format(sum(conserved_motifs.values()))
+    print "50 most *frequent* conserved motifs:"
+    print conserved_motifs.most_common(50)
+
+    print "Top 50 most *conserved* motifs (conserved instances / total instances):"
+    print sorted(conservation_ratios.items(), key=itemgetter(1), reverse=True)[:50]
 
 def main():
-  print "testing"
-  print find_motifs()
+  find_motifs()
 
 if __name__ == "__main__":
   main()
